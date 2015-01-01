b049f56ec103ec686421fe91534ff6b35e60242b
mihirk <mihir.khatwani@gmail.com>
2015-01-01 16:51:33 +0530

    Adding required models

---
+++ src/main/java/model/Chocolate.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Chocolate {
+    private Integer price;
+    private ChocolateType chocolateType;
+
+}
---
+++ src/main/java/model/ChocolateType.java
@@ -0,0 +1,5 @@
+package model;
+
+public enum ChocolateType {
+    MILK, DARK, WHITE, SUGAR_FREE
+}
